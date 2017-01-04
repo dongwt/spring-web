de588b7da6fd49f767427a8f9884e82e0a92d7d7
董 纹陶 [SVR III] <dongwentao@lifang.com>
2017-01-04 17:13:10 +0800

    接收日期类型参数

--- src/main/java/com/dongwt/spring/model/DateObj.java
+++ src/main/java/com/dongwt/spring/model/DateObj.java
@@ -1,6 +1,5 @@
 package com.dongwt.spring.model;
 
-import java.io.Serializable;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -8,9 +7,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Data
-public class DateObj implements Serializable{
-    
-    private static final long serialVersionUID = 1L;
+public class DateObj{
     
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date createDate;
