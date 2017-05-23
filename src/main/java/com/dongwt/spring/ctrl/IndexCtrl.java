4614e4c862bd376b9f99cc15d306765764158c0a
董 纹陶 [SVR III] <dongwentao@lifang.com>
2017-05-23 11:30:29 +0800

    noAppointMethod

--- src/main/java/com/dongwt/spring/ctrl/IndexCtrl.java
+++ src/main/java/com/dongwt/spring/ctrl/IndexCtrl.java
@@ -1,32 +1,33 @@
 package com.dongwt.spring.ctrl;
 
+import com.dongwt.spring.api.response.Response;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.dongwt.spring.service.RedisService;
-
-@Controller
+@RestController
 @RequestMapping("/")
 public class IndexCtrl {
 
-//	@Autowired
-//	private RedisService RedisService;
-	
+
 	@RequestMapping(value="/index",method={RequestMethod.GET})
-	public @ResponseBody Map<String,Object> index(HttpServletRequest request) {
+	public  Map<String,Object> index(HttpServletRequest request) {
 		Map<String,Object> result = new HashMap<String, Object>();
-//		RedisService.save("str", (String) request.getParameter("str"));
 		result.put("status", 0);
 		result.put("msg", "success");
 		return result;
+	}
+
+	@RequestMapping("/noAppointMethod")
+	public Response noAppointMethod(@RequestParam(value = "name",required = false) String name){
+		Response response = Response.success();
+
+		return response;
 	}
 	
 }
